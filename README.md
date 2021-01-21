d49693676023eca802c800384a853fcee0f073f8
Rohini <Rohinikr2004@gmail.com>
2021-01-21 20:57:22 +0000

    readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+## This is a simple project for Mocha tests
+
+### Steps to execute tests
+1. Clone this Repo and extract it
+2. Run the command `npm install` to install dependencies
+3. Execute the command `npm run test`
+
+|#|command|
+|--|--|
+|install|npm install|
+|test|npm run test|
--- specs/assignment.js
+++ specs/assignment.js
@@ -1,17 +1,22 @@
 import chai from 'chai'
 const {assert} = chai
 
-let name = ["Jackson","Rob","Jack","Wilson","John"]
+
 
 
 describe("Tests on Name list",()=>{
+
+
+    let name = ["Jackson","Rob","Jack","Wilson","John"]
+
     it("Assert length of name list is 5", ()=>{
         let len = name.length
         assert.equal(len,5,"Length of list is not equal to 5")
     })
 
-    it("Assert names list contains ‘Rob’", ()=>{
-        assert.include(name,'Rob',"Rob name not included in name list") 
+    it("Assert names list contains ‘Daniel’", ()=>{
+       // assert.include(name,'Rob',"Rob name not included in name list") 
+        assert.isTrue(name.includes('Daniel'))
     })
 
     it("Filter Name that starts with ‘Jack’ and resultant list is of length 2",()=>{
